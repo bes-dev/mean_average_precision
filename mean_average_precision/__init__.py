68e7cb85c183b2e2ca801abdb764ef4fba430544
Moritz Spranger <32243674+m0e33@users.noreply.github.com>
2021-01-31 10:56:37 +0000

    Fix root init

--- mean_average_precision/__init__.py
+++ mean_average_precision/__init__.py
@@ -21,6 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from .mean_average_precision.metric_builder import MetricBuilder
-from .mean_average_precision.mean_average_precision_2d import MeanAveragePrecision2d
-from .mean_average_precision.multiprocessing import MetricMultiprocessing
+from .metric_builder import MetricBuilder
+from mean_average_precision_2d import MeanAveragePrecision2d
+from multiprocessing import MetricMultiprocessing
